7754356012c290ce01c73cc0dee46ae97ad53d26
AlessiaOsman <alessia.osman@gmail.com>
2024-02-12 15:22:41 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fontsource/barlow';
+import '@fontsource-variable/orbitron';
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
