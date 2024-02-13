ceb89dc54b3c0e8ae2fc06a51c0c320fe8c3e575
giamaf <gianluca.maffucci@hotmail.com>
2024-02-13 09:39:39 +0100

    footer's bottom done

--- src/components/footer_components/FooterBottom.vue
+++ src/components/footer_components/FooterBottom.vue
@@ -1,9 +1,42 @@
 <script>
 export default {
     name: 'FooterBottom',
+
+    props: {
+        copyright: Object
+    },
 };
 </script>
 
-<template></template>
+<template>
+    <div class="footer-bottom">
+        <div class="container">
 
-<style scoped></style>
+            <p class="copyright">{{ `${this.copyright.main} ${this.copyright.logo}` }}
+                <a class="company" href="#">{{ this.copyright.company }}.</a>
+                {{ `${this.copyright.text}.` }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../../assets/scss/vars' as *;
+
+.footer-bottom {
+    font-family: $barlow-font;
+    padding: 25px 0;
+    background-color: rgb(30, 41, 61);
+    color: $color-white;
+    text-align: center;
+
+    .copyright {
+        margin: 0
+    }
+
+    .company {
+        color: $color-secondary;
+        text-transform: uppercase;
+    }
+}
+</style>
