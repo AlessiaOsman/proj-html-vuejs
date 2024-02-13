f9ac4cbbc4930f0351c52912269f098d748d7fe0
AlessiaOsman <alessia.osman@gmail.com>
2024-02-13 09:10:34 +0100

    match section data

--- src/components/main_components/MatchSection.vue
+++ src/components/main_components/MatchSection.vue
@@ -1,8 +1,50 @@
 <script>
-export default{
-    name: 'MatchSection'
+export default {
+    name: 'MatchSection',
+    data: ()=>({
+        cards:[
+            {
+                id: 1,
+                firstTeam: 'game-img-1.png',
+                secondTeam: 'game-img-2.png',
+                name: 'Call Of Deauty',
+                date: 'December 25, 2022 4:00 PM'
+            },
+            {
+                id: 1,
+                firstTeam: 'game-img-3.png',
+                secondTeam: 'game-img-4.png',
+                name: 'Legendary Battles',
+                date: 'December 25, 2022 4:00 PM'
+            },
+            {
+                id: 1,
+                firstTeam: 'game-img-5.png',
+                secondTeam: 'game-img-6.png',
+                name: 'Nay Corral Gundown',
+                date: 'December 25, 2022 4:00 PM'
+            },
+            {
+                id: 1,
+                firstTeam: 'game-img-7.png',
+                secondTeam: 'game-img-8.png',
+                name: 'League Of Legends',
+                date: 'December 25, 2022 4:00 PM'
+            },
+        ]
+    })
 }
 </script>
 <template>
-    
+    <section>
+        <div class="container">
+            <h2>Trending Battle</h2>
+            <h1>Keep Eyes & Menage Upcoming Battle</h1>
+            <p>libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras
+                ultricies ligula sed magna dictum porta.</p>
+        </div>
+    </section>
 </template>
+
+<style>
+</style>
