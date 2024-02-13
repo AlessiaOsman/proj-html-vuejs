7faba7fef7d07b06077ffc906ae839fc2bd0d75f
BiagioCaputo <scebiagio98@gmail.com>
2024-02-13 17:59:47 +0100

    corrected the template of the counter section

--- src/components/main_components/CounterSection.vue
+++ src/components/main_components/CounterSection.vue
@@ -31,34 +31,11 @@ export default {
 }
 </script>
 <template>
-    <section id="counter-section">
-        <div class="container counter-container">
-            <MainSection :subTitle="subTitle">
-                <CounterCards :counters="counters" />
-            </MainSection>
-        </div>
-
-    </section>
+    <MainSection :subTitle="subTitle">
+        <CounterCards :counters="counters" />
+    </MainSection>
 </template>
 
 <style scoped lang="scss">
 @use '../../assets/scss/vars' as *;
-
-#counter-section {
-    background-color: black;
-    color: white;
-
-    .counter-container {
-        padding-top: 120px;
-        padding-bottom: 120px;
-    }
-
-    h1 {
-        font-family: $orbitron-font ;
-        font-weight: 700;
-        font-size: 55px;
-
-    }
-
-}
 </style>
