5b117e1e02595368a929307b3e78146cd6830832
AlessiaOsman <alessia.osman@gmail.com>
2024-02-13 19:39:54 +0100

    completed blog section

--- src/AppMain.vue
+++ src/AppMain.vue
@@ -5,10 +5,20 @@ import StreamingSection from './components/main_components/StreamingSection.vue'
 import TestimonialSection from './components/main_components/TestimonialSection.vue';
 import BlogSection from './components/main_components/BlogSection.vue';
 import MatchCard from './components/main_components/MatchCard.vue';
-import MainSection from './components/main_components/MainSection.vue'
+import MainSection from './components/main_components/MainSection.vue';
+import BlogCard from './components/main_components/BlogCard.vue';
 export default {
     name: 'AppMain',
-    components: { FeaturesSection, CounterSection, StreamingSection, TestimonialSection, BlogSection, MainSection, MatchCard },
+    components: { 
+        FeaturesSection, 
+        CounterSection, 
+        StreamingSection, 
+        TestimonialSection, 
+        BlogSection, 
+        MainSection, 
+        MatchCard,
+        BlogCard
+     },
     data: () => ({
         cards: [
             {
@@ -41,7 +51,8 @@ export default {
             },
         ],
         posts: [
-            {   id: 1,
+            {
+                id: 1,
                 image: 'blog-item-1.png',
                 reading_time: '10 Min',
                 views: '240',
@@ -76,8 +87,9 @@ export default {
                 title: 'Benefits Of Gaming - PC And Video Games'
 
             },
-        ]
-    })
+        ],
+
+    }),
 }
 </script>
 <template>
@@ -86,14 +98,22 @@ export default {
         <MainSection sectionBgColor="blue" title="Trending Battle" subTitle="Keep Eyes & Manage Upcoming Battle"
             paragraph="libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta.">
             <div class="cards-container row mx-auto">
-            <MatchCard v-for="card in cards" :item="card" />
-            </div>  
+                <MatchCard v-for="card in cards" :item="card" />
+            </div>
         </MainSection>
         <CounterSection />
         <StreamingSection />
         <TestimonialSection />
         <MainSection title="Latest News" subTitle="Stay Updated With Our Latest News Articles"
             paragraph="libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta.">
+            <div class="posts-container row mx-auto">
+                <div class="left-side col-6">
+                    <BlogCard :post="posts[0]"/>
+                </div>
+                <div class="right-side col-6">
+                   <BlogCard :class="{'flex': post.id !== 1, 'none': post.id === 1}" v-for="post in posts" :post="post"/>
+                </div>
+            </div>
         </MainSection>
     </main>
 </template>
--- src/components/main_components/BlogCard.vue
+++ src/components/main_components/BlogCard.vue
@@ -3,7 +3,7 @@
 export default {
     name: 'BlogCard',
     props: {
-        posts: Array
+        post: Object
     },
 
     methods: {
@@ -21,16 +21,90 @@ export default {
 }
 </script>
 <template>
-    <div class="posts-container row mx-auto">
-        <div v-for="post in posts" :key="post.id">
-            <div class="post-card" col-6>
-                
+    <div class="post-card">
+        <div class="post-card-image">
+            <img :src="createImagePath(post.image)" alt="image">
+        </div>
+
+        <div class="post-card-info p-4">
+            <div class="d-flex gap-3 mb-4">
+                <span>
+                    <img :src="createIconPath('clock.png')" alt="reading time">
+                    {{ post.reading_time }}
+                </span>
+                <span>
+                    <img :src="createIconPath('eye.png')" alt="views">
+                    {{ post.views }}
+                </span>
+                <span>
+                    <img :src="createIconPath('calendar.png')" alt="date">
+                    {{ post.date }}
+                </span>
             </div>
+            <a href="#">
+                <h4>{{ post.title }}</h4>
+            </a>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../../assets/scss/_vars.scss' as *;
+
+.post-card {
+    background-color: #283449;
+    border-radius: 20px;
+
+    &.flex {
+        display: flex;
+        margin-bottom: 20px;
+
+        .post-card-image {
+            img {
+                height: 100%;
+                width: auto;
+            }
+        }
+
+        h4 {
+            font-size: 24px;
+        }
+    }
+
+    &.none {
+        display: none;
+    }
+
+    .post-card-image {
+        img {
+            width: 100%;
+            border-radius: 20px;
+        }
+    }
+
+
+
+    span {
+        color: $color-secondary;
+        display: inline-flex;
+        align-items: center;
+        margin-right: 25px;
+        font-family: $barlow-font;
+
+
+        img {
+            max-width: 100%;
+            height: auto;
+            margin-right: 10px;
+        }
+    }
+
+    h4 {
+        font-family: $orbitron-font;
+        color: $color-white;
+        font-weight: bold;
+        font-size: 32px;
+    }
+}
 </style>
 
