05bc31ee4393ba643b9e9e9989a90e80ee582334
giamaf <gianluca.maffucci@hotmail.com>
2024-02-13 09:41:57 +0100

    added footer' data components

--- src/AppFooter.vue
+++ src/AppFooter.vue
@@ -1,16 +1,31 @@
 <script>
+import { footerData } from './data/footer_data';
 import NewsLetters from './components/footer_components/NewsLetters.vue';
 import FooterTop from './components/footer_components/FooterTop.vue';
 import FooterBottom from './components/footer_components/FooterBottom.vue';
-export default{
+const { left, right, contacts, copyright, newsletter } = footerData;
+export default {
     name: 'AppFooter',
-    components: {NewsLetters, FooterTop, FooterBottom}
+
+    data: () => ({
+        left,
+        right,
+        contacts,
+        copyright,
+        newsletter
+    }),
+
+    components: { NewsLetters, FooterTop, FooterBottom }
 }
 </script>
 <template>
     <footer>
-        <NewsLetters/>
-        <FooterTop/>
-        <FooterBottom/>
+        <NewsLetters :newsletter="newsletter" />
+        <FooterTop :left="left" :right="right" :contacts="contacts" />
+        <FooterBottom :copyright="copyright" />
     </footer>
 </template>
+
+<style lang="scss" scoped>
+@use './assets/scss/vars' as *;
+</style>
