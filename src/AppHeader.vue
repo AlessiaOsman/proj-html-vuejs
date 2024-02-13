8376bfbe7ec8a3d6ce50a1d3340e1f73f92867b1
AlessiaOsman <alessia.osman@gmail.com>
2024-02-13 12:03:29 +0100

    style fix

--- src/AppHeader.vue
+++ src/AppHeader.vue
@@ -21,9 +21,8 @@ export default {
 <style scoped lang="scss">
 #header {
     background-image: url(./assets/img/index-banner-bg.png);
-    height: 1400px;
     background-repeat: no-repeat;
-
+    background-size: cover;
     padding: 25px 0;
 }
 </style>
